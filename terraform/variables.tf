3b0db2667bf8ae1b1248207a7cc884d3f9e4a7d2
Troy Sellers <768991+troysellers@users.noreply.github.com>
2022-09-29 14:44:51 +1000

    postgres terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aiven_api_token" {default = "aazWceUebE57LvDaTrUH7bC8JeYMZKA/qj1NbSYeP3Lj8Fx685eUXilAh1RP7MnxDUYFyTktsATYO8Akr0Aap7cgdQ+OksL106pzBvbb5mAKE01CUnZh5L7pPawYtOlKHEbBBLOf/8vYIPr71vXkxClXUzt1l4TwyOQdP1+godKSXME/0zyzqVS67HjRiaNcQfW+3r2+9j5KNxW98VDpp3jB+tvcuQ5bWlYwjzJqR/TXFKBi1bi9JMUn0dx1Rn+MZwZE1T4RJ0cZ5tm/HFnkLSRv9sS4iLHN6g+8VxuVc52vlCreMr6EFjV4MHi1BZlANmev4lW+VNGbDLeArE1v11LCHQGyRJHlhygw6MIvn3LhMFwF"}
+variable "cloud_name" {default = "google-asia-southeast1"}
+variable "project_name" {default = "tsellers-demo"}
+variable "pg-service-name" {default = "postgres-db"}
